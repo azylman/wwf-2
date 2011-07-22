32dc9d91e7db7702b54403397351f4e44670f3f2
Alex Zylman <alex@zylman.com>
2011-07-21 19:42:09 -0600

    cleaned up event handlers

--- src/com/zylman/wwf/client/WWFSolver2.java
+++ src/com/zylman/wwf/client/WWFSolver2.java
@@ -6,23 +6,19 @@ import java.util.List;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.ColumnSortEvent.ListHandler;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.ListDataProvider;
 import com.zylman.wwf.shared.Result;
@@ -128,67 +124,31 @@ public class WWFSolver2 implements EntryPoint {
 		results.getColumnSortList().push(scoreColumn);
 		results.setPageSize(999999);
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
-
 		// Focus the cursor on the name field when the app loads
 		rack.setFocus(true);
-		rack.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class SolveHandler implements ClickHandler, KeyUpHandler {
-			public void onClick(ClickEvent event) {
-				getAnagrams();
-			}
-
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					getAnagrams();
-				}
-			}
-		}
-
-		class TestHandler implements KeyUpHandler {
-			public void onKeyUp(KeyUpEvent event) {
-				testWord();
-			}
-		}
 
 		RootLayoutPanel.get().add(ui);
+	}
 
-		// Add a handler to send the name to the server
-		SolveHandler solveHandler = new SolveHandler();
-		TestHandler testHandler = new TestHandler();
-		sendButton.addClickHandler(solveHandler);
-		rack.addKeyUpHandler(solveHandler);
-		test.addKeyUpHandler(testHandler);
+	@UiHandler("sendButton")
+	void handleClick(ClickEvent e) {
+		getAnagrams();
+	}
+
+	@UiHandler("rack")
+	void handleRackKeyUp(KeyUpEvent event) {
+		if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+			getAnagrams();
+		}
+	}
+
+	@UiHandler("test")
+	void handleTestKeyUp(KeyUpEvent event) {
+		if (test.getText().isEmpty()) {
+			testResults.setText("");
+		} else {
+			testWord();
+		}
 	}
 
 	private void getAnagrams() {
