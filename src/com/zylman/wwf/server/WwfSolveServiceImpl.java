22241b9c6851531806c8c21c4d117612f2bc1eed
Alex Zylman <alex.zylman@gmail.com>
2011-07-22 19:12:38 -0600

    suppress unnecessary warnings

--- src/com/zylman/wwf/server/WwfSolveServiceImpl.java
+++ src/com/zylman/wwf/server/WwfSolveServiceImpl.java
@@ -9,6 +9,7 @@ import com.zylman.wwf.client.WwfSolveService;
 import com.zylman.wwf.shared.Result;
 import com.zylman.wwf.shared.SolveResult;
 
+@SuppressWarnings("serial")
 public class WwfSolveServiceImpl extends RemoteServiceServlet implements WwfSolveService {
 
 	public Result findAnagrams(String rack, String start, String contains, String end) {
--- src/com/zylman/wwf/server/WwfWordTestServiceImpl.java
+++ src/com/zylman/wwf/server/WwfWordTestServiceImpl.java
@@ -4,7 +4,7 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.zylman.wwf.client.WwfWordTestService;
 import com.zylman.wwf.shared.Result;
 
-
+@SuppressWarnings("serial")
 public class WwfWordTestServiceImpl extends RemoteServiceServlet implements WwfWordTestService {
 	public Result testWord(String word) {
 		Dict dict = DictWrapper.get();
